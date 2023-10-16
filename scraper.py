4361c1f9e8c02882571bebe043c64ad0284dcb35
Syed F Ahmad <furquan@protonmail.com>
2023-10-16 23:40:40 +0530

    Added the scraper skeleton code with methods to get the current location using ip and to encode the indeed URL

---
+++ scraper.py
@@ -0,0 +1,27 @@
+import requests as req
+import re
+import json
+from urllib.parse import urlencode
+
+
+def get_current_ip():
+    res = req.get('https://api.ipify.org?format=json').json()
+    return res["ip"]
+
+
+def get_location_ip(ipaddr):
+    res = req.get(f'https://ipinfo.io/{ipaddr}/json').json()
+    return {'ip': ipaddr,
+            'city': res.get('city'),
+            'state': res.get('region'),
+            'country': res.get('country')
+            }
+
+
+def get_indeed_search_url(keyword, location, offset=0):
+    parameters = {"q": keyword, "l": location, "filter": 0, "start": offset}
+    return "https://www.indeed.com/jobs?" + urlencode(parameters)
+
+
+headers = {
+    "User-Agent": "Mozilla/5.0 (X11; Linux i686; rv:109.0) Gecko/20100101 Firefox/118.0"}

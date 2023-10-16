99b7645ad90b9f4d8afbb09e38676825a1b6d87b
Syed F Ahmad <furquan@protonmail.com>
2023-10-16 23:48:35 +0530

    Use in.indeed.com rather than www.indeed.com

--- scraper.py
+++ scraper.py
@@ -20,7 +20,7 @@ def get_location_ip(ipaddr):
 
 def get_indeed_search_url(keyword, location, offset=0):
     parameters = {'q': keyword, 'l': location, 'filter': 0, 'start': offset}
-    return 'https://www.indeed.com/jobs?' + urlencode(parameters)
+    return 'https://in.indeed.com/jobs?' + urlencode(parameters)
 
 
 headers = {

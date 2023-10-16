2604deaf23afa1e9e4b8a6a28e58e2110f22bd81
Syed F Ahmad <furquan@protonmail.com>
2023-10-17 00:02:30 +0530

    Added the main loop to fetch job IDs from the page (might need some rework with the headers as the request 403s)

--- scraper.py
+++ scraper.py
@@ -24,11 +24,34 @@ def get_indeed_search_url(keyword, location, radius, offset=0):
     return 'https://in.indeed.com/jobs?' + urlencode(parameters)
 
 
+job_ids = []
 headers = {
     'User-Agent': 'Mozilla/5.0 (X11; Linux i686; rv:109.0) Gecko/20100101 Firefox/118.0'}
-
-job_ids = []
 keyword = 'python'
 location = {'city': 'Patna', 'state': 'Bihar'}
-print(get_indeed_search_url(
+print('Using', get_indeed_search_url(
     keyword, f"{location.get('city')}, {location.get('state')}", 100))
+
+for offset in range(0, 1010, 10):
+    try:
+        indeed_jobs_url = get_indeed_search_url(
+            keyword, f"{location.get('city')}, {location.get('state')}", 100, offset)
+        res = req.get(indeed_jobs_url, headers=headers)
+
+        if res.status_code == 200:
+            script_tag = re.findall(
+                r'window.mosaic.providerData\["mosaic-provider-jobcards"\]=(\{.+?\});', res.text)
+            if script_tag is not None:
+                json_blob = json.loads(script_tag[0])
+                jobs_list = json_blob['metaData']['mosaicProviderJobCardsModel']['results']
+                for index, job in enumerate(jobs_list):
+                    if job.get('jobkey') is not None:
+                        job_ids.append(job.get('jobkey'))
+
+                if len(jobs_list) < 10:
+                    break
+
+    except Exception as e:
+        print('An error occurred while fetching job IDs:', e)
+
+print(job_ids)

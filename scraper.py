f4fb8526983c60beb115a3e8d7089bc36fe13962
Syed F Ahmad <furquan@protonmail.com>
2023-10-17 00:43:29 +0530

    Removed the loop to fetch job ids and instead fetch job details directly from the extracted JSON blob

--- scraper.py
+++ scraper.py
@@ -26,7 +26,7 @@ def get_indeed_search_url(keyword, location, radius, offset=0):
 
 
 live_cookie = os.getenv('INDEED_SCRAPER_COOKIE')
-job_ids = []
+jobs = []
 headers = {
     'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/118.0',
     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
@@ -47,28 +47,42 @@ location = {'city': 'Patna', 'state': 'Bihar'}
 print('Using', get_indeed_search_url(
     keyword, f"{location.get('city')}, {location.get('state')}", 100))
 print('Using headers', headers)
-"""
-for offset in range(0, 1010, 10):
-    try:
-        indeed_jobs_url = get_indeed_search_url(
-            keyword, f"{location.get('city')}, {location.get('state')}", 100, offset)
-        res = req.get(indeed_jobs_url, headers=headers)
-        print('response was', res)
-        if res.status_code == 200:
-            script_tag = re.findall(
-                r'window.mosaic.providerData\["mosaic-provider-jobcards"\]=(\{.+?\});', res.text)
-            if script_tag is not None:
-                json_blob = json.loads(script_tag[0])
-                jobs_list = json_blob['metaData']['mosaicProviderJobCardsModel']['results']
-                for index, job in enumerate(jobs_list):
-                    if job.get('jobkey') is not None:
-                        job_ids.append(job.get('jobkey'))
 
-                if len(jobs_list) < 10:
-                    break
+try:
+    indeed_jobs_url = get_indeed_search_url(
+        keyword, f"{location.get('city')}, {location.get('state')}", 100)
+    res = req.get(indeed_jobs_url, headers=headers)
+    print('response was', res)
+    if res.status_code == 200:
+        script_tag = re.findall(
+            r'window.mosaic.providerData\["mosaic-provider-jobcards"\]=(\{.+?\});', res.text)
+        if script_tag is not None:
+            json_blob = json.loads(script_tag[0])
+            jobs_list = json_blob['metaData']['mosaicProviderJobCardsModel']['results']
+            for index, job in enumerate(jobs_list):
+                if job.get('jobkey') is not None:
+                    jobs.append({
+                        'id': job.get('jobkey'),
+                        'keyword': keyword,
+                        'location': location,
+                        # 'page': round(offset / 10) + 1 if offset > 0 else 1,
+                        'position': index,
+                        'company': job.get('company'),
+                        'companyRating': job.get('companyRating'),
+                        'companyReviewCount': job.get('companyReviewCount'),
+                        'jobTitle': job.get('title'),
+                        'jobLocationCity': job.get('jobLocationCity'),
+                        'jobLocationState': job.get('jobLocationState'),
+                        'maxSalary': job.get('extractedSalary').get('max') if job.get('extractedSalary') is not None else 0,
+                        'minSalary': job.get('extractedSalary').get('min') if job.get('extractedSalary') is not None else 0,
+                        'salaryType': job.get('extractedSalary').get('type') if job.get('extractedSalary') is not None else 'none',
+                        'pubDate': job.get('pubDate'),
+                    })
 
-    except Exception as e:
-        print('An error occurred while fetching job IDs:', e)
+            if len(jobs_list) < 10:
+                print('err')
+    else:
+        print('Error: got response %d' % res.status_code)
 
-print(job_ids)
-"""
+except Exception as e:
+    print('An error occurred while fetching job IDs:', e)

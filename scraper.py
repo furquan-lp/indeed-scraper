4b9e56ac7ef2be22373fe8cc272a420d255a746f
Syed F Ahmad <furquan@protonmail.com>
2023-11-14 22:43:50 +0530

    Remove brotli compression from encoding header so the response output is not garbled

--- scraper.py
+++ scraper.py
@@ -34,7 +34,7 @@ headers = {
     'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/119.0',
     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
     'Accept-Language': 'en-GB,en;q=0.5',
-    'Accept-Encoding': 'gzip, deflate, br',
+    'Accept-Encoding': 'gzip, deflate',
     'DNT': '1',
     'Referer': 'https://in.indeed.com/',
     'Connection': 'keep-alive',

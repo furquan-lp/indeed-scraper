dcff1129fcc323dac1ccb81240135aa701500490
Syed F Ahmad <furquan@protonmail.com>
2023-11-16 01:15:42 +0530

    Send the error code as response if returned by scrape_indeed_jobs

--- api.py
+++ api.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Response, status
 from pydantic import BaseModel
 from scraper import scrape_indeed_jobs
 
@@ -15,9 +15,13 @@ async def root():
     return {'res': '200 OK'}
 
 
-@app.get('/jobs/{keyword}')
-async def find_jobs(keyword: str, location: dict[str, str], scraper_header: ScraperHeader):
+@app.get('/jobs/{keyword}', status_code=200)
+async def find_jobs(keyword: str, location: dict[str, str], scraper_header: ScraperHeader, response: Response):
     scraper_result: [] | int = scrape_indeed_jobs(
         keyword, location, scraper_header.indeed_header_cookie)
     print(scraper_result)
-    return {'keyword': keyword, 'loc': location}
+    if type(scraper_result) is int:
+        response.status_code = scraper_result
+        return {'error': 'Scraper Error'}
+    else:
+        return {'keyword': keyword, 'loc': location}

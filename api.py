ef3d28baa510d97105617cf45add86465f0b9a34
Syed F Ahmad <furquan@protonmail.com>
2023-11-16 12:39:52 +0530

    Use HTTPException instead of custom Response messages for throwing error codes

--- api.py
+++ api.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Response, status
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from scraper import scrape_indeed_jobs
 
@@ -15,13 +15,14 @@ async def root():
     return {'res': '200 OK'}
 
 
-@app.get('/jobs/{keyword}', status_code=200)
-async def find_jobs(keyword: str, location: dict[str, str], scraper_header: ScraperHeader, response: Response):
+@app.get('/jobs/{keyword}')
+async def find_jobs(keyword: str, location: dict[str, str], scraper_header: ScraperHeader):
     scraper_result: [] | int = scrape_indeed_jobs(
         keyword, location, scraper_header.indeed_header_cookie)
     print(scraper_result)
     if type(scraper_result) is int:
-        response.status_code = scraper_result
-        return {'error': 'Scraper Error'}
+        raise HTTPException(status_code=scraper_result, detail="Scraper Error")
+    elif not scraper_result:
+        raise HTTPException(status_code=404, detail="Jobs not found")
     else:
         return {'keyword': keyword, 'loc': location}

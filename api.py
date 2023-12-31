e30654875c1598ac66bed963a6bffe9f9a5b6fd6
Syed F Ahmad <furquan@protonmail.com>
2023-12-31 22:09:25 +0530

    Added the user agent to ScraperHeader

--- api.py
+++ api.py
@@ -5,6 +5,7 @@ from scraper import scrape_indeed_jobs
 
 class ScraperHeader(BaseModel):
     indeed_header_cookie: str
+    indeed_user_agent: str
 
 
 app = FastAPI()
@@ -18,8 +19,7 @@ async def root():
 @app.get('/jobs/{keyword}')
 async def find_jobs(keyword: str, scraper_header: ScraperHeader, location: dict[str, str] | None = None):
     scraper_result: list | int = scrape_indeed_jobs(
-        keyword, location, scraper_header.indeed_header_cookie)
-    print(scraper_result)
+        keyword, location, cookie=scraper_header.indeed_header_cookie, user_agent=scraper_header.indeed_user_agent)
     if type(scraper_result) is int:
         raise HTTPException(status_code=scraper_result, detail="Scraper Error")
     elif not scraper_result:

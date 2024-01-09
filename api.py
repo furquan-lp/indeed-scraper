e3cd4e92000bf36d6ea7f3b32a4e7eac73fd5896
Syed F Ahmad <furquan@protonmail.com>
2024-01-09 20:57:45 +0530

    find_jobs: Use the request client IP if the optional location parameter is not passed

--- api.py
+++ api.py
@@ -1,5 +1,5 @@
 import requests as req
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Request
 from pydantic import BaseModel
 from scraper import scrape_indeed_jobs
 
@@ -17,7 +17,7 @@ def get_server_ip():
     return res["ip"]
 
 
-def get_location_ip(ipaddr: str):
+def get_ip_location(ipaddr: str):
     res = req.get(f'https://ipinfo.io/{ipaddr}/json').json()
     return {'ip': ipaddr,
             'city': res.get('city'),
@@ -32,9 +32,14 @@ async def root():
 
 
 @app.get('/jobs/{keyword}')
-async def find_jobs(keyword: str, scraper_header: ScraperHeader, location: dict[str, str] | None = None):
+async def find_jobs(keyword: str, request: Request, scraper_header: ScraperHeader, location: dict[str, str] | None = None):
+    client_ip: str | None = None if request.client is None else request.client.host
+    loc = location
+    if client_ip is not None and location is None:
+        ip_loc = get_ip_location(client_ip)
+        loc = {'city': ip_loc.get('city'), 'state': ip_loc.get('state')}
     scraper_result: list | int = scrape_indeed_jobs(
-        keyword, location, cookie=scraper_header.indeed_header_cookie, user_agent=scraper_header.indeed_user_agent)
+        keyword, loc, cookie=scraper_header.indeed_header_cookie, user_agent=scraper_header.indeed_user_agent)
     if type(scraper_result) is int:
         raise HTTPException(status_code=scraper_result, detail="Scraper Error")
     elif not scraper_result:

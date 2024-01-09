b233069ce055a3352b79c40e97f38e5fee6f49e9
Syed F Ahmad <furquan@protonmail.com>
2024-01-10 01:42:03 +0530

    Set logging False by default instead of None

--- api.py
+++ api.py
@@ -7,7 +7,7 @@ from scraper import scrape_indeed_jobs
 class ScraperHeader(BaseModel):
     indeed_header_cookie: str
     indeed_user_agent: str
-    logging: bool | None = None
+    logging: bool = False
 
 
 app = FastAPI()
@@ -39,8 +39,9 @@ async def find_jobs(keyword: str, request: Request, scraper_header: ScraperHeade
     if client_ip is not None and location is None:
         ip_loc = get_ip_location(client_ip)
         loc = {'city': ip_loc.get('city'), 'state': ip_loc.get('state')}
-    scraper_result: list | int = scrape_indeed_jobs(
-        keyword, loc, True, cookie=scraper_header.indeed_header_cookie, user_agent=scraper_header.indeed_user_agent)
+    scraper_result: list | int = scrape_indeed_jobs(keyword, loc, scraper_header.logging,
+                                                    cookie=scraper_header.indeed_header_cookie,
+                                                    user_agent=scraper_header.indeed_user_agent)
     if type(scraper_result) is int:
         raise HTTPException(status_code=scraper_result, detail="Scraper Error")
     elif not scraper_result:

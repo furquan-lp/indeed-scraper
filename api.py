d29dc00d37384e29c9ef61adddedb6457d25331d
Syed F Ahmad <furquan@protonmail.com>
2024-01-09 22:48:34 +0530

    Added optional logging parameter (to enable logging) to ScraperHeader body parameter

--- api.py
+++ api.py
@@ -7,6 +7,7 @@ from scraper import scrape_indeed_jobs
 class ScraperHeader(BaseModel):
     indeed_header_cookie: str
     indeed_user_agent: str
+    logging: bool | None = None
 
 
 app = FastAPI()
@@ -20,10 +21,9 @@ def get_server_ip():
 def get_ip_location(ipaddr: str):
     res = req.get(f'https://ipinfo.io/{ipaddr}/json').json()
     return {'ip': ipaddr,
-            'city': res.get('city'),
-            'state': res.get('region'),
-            'country': res.get('country')
-            }
+            'city': res.get('city', ''),
+            'state': res.get('region', ''),
+            'country': res.get('country', '')}
 
 
 @app.get('/')
@@ -32,14 +32,15 @@ async def root():
 
 
 @app.get('/jobs/{keyword}')
-async def find_jobs(keyword: str, request: Request, scraper_header: ScraperHeader, location: dict[str, str] | None = None):
+async def find_jobs(keyword: str, request: Request, scraper_header: ScraperHeader,
+                    location: dict[str, str] | None = None):
     client_ip: str | None = None if request.client is None else request.client.host
     loc = location
     if client_ip is not None and location is None:
         ip_loc = get_ip_location(client_ip)
         loc = {'city': ip_loc.get('city'), 'state': ip_loc.get('state')}
     scraper_result: list | int = scrape_indeed_jobs(
-        keyword, loc, cookie=scraper_header.indeed_header_cookie, user_agent=scraper_header.indeed_user_agent)
+        keyword, loc, True, cookie=scraper_header.indeed_header_cookie, user_agent=scraper_header.indeed_user_agent)
     if type(scraper_result) is int:
         raise HTTPException(status_code=scraper_result, detail="Scraper Error")
     elif not scraper_result:

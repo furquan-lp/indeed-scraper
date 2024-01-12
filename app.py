d76e35e7d9d539ffabfbce4305bb68825e4c49e9
Syed F Ahmad <furquan@protonmail.com>
2024-01-13 01:09:41 +0530

    Added mongodb connection code

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 import os
 import json
+import pymongo
 from sys import stderr
 from dotenv import load_dotenv
 from scraper import scrape_indeed_jobs
+from pymongo import MongoClient
 
 load_dotenv()
 live_cookie = os.environ['INDEED_SCRAPER_COOKIE']
+user_agent = os.environ['INDEED_SCRAPER_USER_AGENT']
 location_json = os.environ['INDEED_SCRAPER_LOCATION_JSON']
 keywords_json = os.environ['INDEED_SCRAPER_KEYWORDS_JSON']
 
@@ -19,6 +22,18 @@ def load_json_file(json_file: str):
             f'Error: File {os.path.realpath(__file__)}/{json_file} not found.', file=stderr)
 
 
+client: MongoClient = MongoClient(
+    os.environ['INDEED_SCRAPER_DB_URI'], tls=True, tlsCertificateKeyFile=os.environ['INDEED_SCRAPER_DB_CERT'])
+
+try:
+    client.admin.command('ping')
+    print("Pinged database. Successfully connected to MongoDB.")
+except Exception as e:
+    print(e)
+
+db = client.indeed_scrape
+
+
 locations: dict[str, list[str]] = load_json_file(location_json)
 states: list[str] = locations.get('states', [])
 cities: list[str] = locations.get('capitalCities', [])
@@ -27,22 +42,12 @@ keywords: list[str] = load_json_file(keywords_json)
 
 for keyword in keywords:
     for state, city in zip(states, cities):
-        print(f'Searching "{keyword}" for {city}, {state}')
+        print(f'Searching "{keyword}" for {city}, {state}...')
 
-""" loc = {'city': '', 'state': 'West Bengal'}
-js = scrape_indeed_jobs('java', loc, True, cookie=live_cookie,
-                        user_agent='Mozilla/5.0 (X11; Linux x86_64; rv:121.0) Gecko/20100101 Firefox/121.0')
-print('fetched jobs:')
 
-if isinstance(js, int):
-    print('Error', js)
-else:
-    for j in js:
-        print(j) """
-
-'''
-collection_name = db['portfolios']
+""" collection_name = db['indeed_scrape']
 item_details = collection_name.find()
 for item in item_details:
-    print(item)
-'''
+    print(item) """
+
+client.close()
